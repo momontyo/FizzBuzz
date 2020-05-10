383ea38742385c53bbc21c0b00337274a364b868
Yoshimasa Ohno <momontyo@gmail.com>
2020-05-10 16:45:39 +0900

    Add output Github (#1)
    * Add output Github

--- main.rs
+++ main.rs
@@ -1,7 +1,19 @@
 fn main() {
 
     for num in (1..100)  {
-        if num % 15 == 0 {
+        let stri: String = num.to_string();
+
+        let mut hasSeven = false;
+        for c in stri.chars() {
+            if c == '7' {
+                hasSeven = true;
+                break;
+            }
+        }
+
+        if hasSeven {
+            println!("Github");
+        } else if num % 15 == 0 {
             println!("FizzBuzz");
         } else if num % 3 == 0 {
             println!("Fizz");
